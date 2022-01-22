5a6ef5e7ae9e6b0de448e9009f2030d7d4f8afe7
Chris Smithson <csmithson12345@yahoo.com>
2022-01-21 19:46:25 -0500

    add workflow for first chapter.

--- chapter1/listing1.1/main.tf
+++ chapter1/listing1.1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "helloworld" {
-  ami           = "ami-09dd2e08d601bff67"
+  ami           = "ami-0730adff5a378dbfc"
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
