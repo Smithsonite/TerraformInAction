9e9f5ad2059c07a45e4782449cb73b0552418a14
Chris Smithson <csmithson12345@yahoo.com>
2022-01-21 20:49:53 -0500

    no quotes when calling a data value.

--- TF/main.tf
+++ TF/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "helloworld" {
-  ami           = "data.aws_ami.ubuntu.id"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
