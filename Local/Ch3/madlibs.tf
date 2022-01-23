bf60f221635a3b360631342633b33a4b93303814
Chris Smithson <csmithson12345@yahoo.com>
2022-01-23 12:16:46 -0500

    start ch4

--- Local/Ch3/madlibs.tf
+++ Local/Ch3/madlibs.tf
@@ -11,4 +11,39 @@ variable "words" {
         adverbs     = list(string),
         numbers     = list(number),
         })
+    validation {
+        condition   = length(var.words["nouns"]) >= 7
+        error_message = "At least 20 nouns must be supplied."
+    }
+}
+
+resource "random_shuffle" "random_nouns" {
+    input = var.words["nouns"]
+}
+
+resource "random_shuffle" "random_adjectives" {
+    input = var.words["adjectives"]
+}
+
+resource "random_shuffle" "random_verbs" {
+    input = var.words["verbs"]
+}
+
+resource "random_shuffle" "random_adverbs" {
+    input = var.words["adverbs"]
+}
+
+resource "random_shuffle" "random_numbers" {
+    input = var.words["numbers"]
+}
+
+output "mad_libs" {
+    value = templatefile("${path.module}/templates/alice.txt",
+    {
+        nouns      = random_shuffle.random_nouns.result
+        adjectives = random_shuffle.random_adjectives.result
+        verbs      = random_shuffle.random_verbs.result
+        adverbs    = random_shuffle.random_adverbs.result
+        numbers    = random_shuffle.random_numbers.result
+    })
 }
