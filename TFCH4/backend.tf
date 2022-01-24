94eb30ea1515b8f87326988614f6b274df7b46c3
Chris Smithson <csmithson12345@yahoo.com>
2022-01-23 19:53:05 -0500

    ready to test chapter 4 through GH and Terraform Cloud.

---
+++ TFCH4/backend.tf
@@ -0,0 +1,24 @@
+terraform {
+  cloud {
+    organization = "Smithsonite"
+
+    workspaces {
+      name = "TerraformInActionCH4"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.73.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    cloudinit = {
+      source = "hasicorp/cloudinit"
+      version "~> 2.1"
+    }
+  }
+}
+
